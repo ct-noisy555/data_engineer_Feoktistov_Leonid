ccdd7cbe139532df1d279e208ce195276ff9547c
Leon Noisy <ileonf555@gmail.com>
2026-03-15 16:53:04 +1000

    changed the idea of generating registrations from the user's count to generating users from registration's number

--- data_generator_script.py
+++ data_generator_script.py
@@ -29,30 +29,12 @@ topic_errors_min = 2
 #функция генерации пользователей
 
 def generate_users():
-    users_count = random.randint(130, 300)
-
-    registration_per_day = []
+    users_regs = []
     for day in range(delta):
-        registrations = users_activity_min + random.randint(0, 3)  # Генерируем от 5 до 8 регистраций в день
-        registration_per_day.append(registrations)
+        users_regs.append(random.randint(5, 10))  # генерация количества регистраций пользователей в день
+
     
-    total_registrations = sum(registration_per_day)
-    if total_registrations < users_count:   # Если общее количество регистраций меньше, чем нужно, добавляем дополнительные регистрации
-        registrations_to_add = users_count - total_registrations
-        for day in range(delta):
-            if registrations_to_add <= 0:
-                break
-            additional_registrations = min(registration_per_day[day] + random.randint(0, 3), users_activity_min + 3) - registration_per_day[day]
-            registration_per_day[day] += additional_registrations
-            registrations_to_add -= additional_registrations
-    elif total_registrations > users_count:  # Если общее количество регистраций больше, чем нужно, уменьшаем регистрации
-        registrations_to_remove = total_registrations - users_count
-        for day in range(delta):
-            if registrations_to_remove <= 0:
-                break
-            removable_registrations = min(registration_per_day[day] - users_activity_min, registrations_to_remove)
-            registration_per_day[day] -= removable_registrations
-            registrations_to_remove -= removable_registrations
+
 
 
 
